276a2408dc84a68899d2ed093d12e95fa965a76c
Ahmet Faruk Kurtkadiroğlu <afkurtkadiroglu@gmail.com>
2025-09-14 18:15:48 +0300

    14-9-25-2

--- lib/data/change_settings.dart
+++ lib/data/change_settings.dart
@@ -22,7 +22,7 @@ import 'dart:io';
 
 class ChangeSettings with ChangeNotifier {
   static late SharedPreferences _settings;
-  String version = '1.5.1';
+  String version = '1.5.2';
 
   bool otoLocal = false;
 
